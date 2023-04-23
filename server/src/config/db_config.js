84084d4a23f47a31be7e6eca661714ee85e5b6d1
Lucrada <iamdracula.dracula@gmail.com>
2023-04-24 00:36:38 +0530

    updated db_config.js

--- server/src/config/db_config.js
+++ server/src/config/db_config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const schemas = require('./schema')
 
 const _connect = async () => {
-    await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+    await mongoose.connect(process.env.MONGODB_URI || `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
 }
 
 const getModels = async () => {
