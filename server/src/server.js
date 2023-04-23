67baec3dd25ed5dd530ff1cfd0cbb991583960da
Lucrada <iamdracula.dracula@gmail.com>
2023-04-23 17:35:50 +0530

    changed server port

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const app = express()
 
 app.use(cors())
